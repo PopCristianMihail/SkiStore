caf65251bd2e68f1a6f8a2db05fe386ccc92deea
Cristi <popcristianmihail@gmail.com>
2025-01-03 18:42:43 +0200

    Initial commit

---
+++ api/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Product> Products { get; set; }
+    }
+}
