c057eed6cdac621d31edc14da13ae5ea390af9f1
Cristi <popcristianmihail@gmail.com>
2025-01-13 23:02:23 +0200

    Added the client side

--- api/Program.cs
+++ api/Program.cs
@@ -6,15 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5001");
+});
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);
